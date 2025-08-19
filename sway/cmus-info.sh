3982adf2f45adbb3a7905cf61516bdca70553d97
jpBane <54914492+jpBane@users.noreply.github.com>
2025-08-19 22:44:33 +0200

    Create cmus-info.sh

---
+++ sway/cmus-info.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Get cmus output
+info=$(cmus-remote -Q 2>/dev/null)
+
+# Extract artist and title
+artist=$(echo "$info" | grep '^tag artist' | cut -d ' ' -f 3-)
+title=$(echo "$info" | grep '^tag title' | cut -d ' ' -f 3-)
+status=$(echo "$info" | grep '^status' | cut -d ' ' -f 2)
+
+# Only show if playing
+if [ "$status" = "playing" ]; then
+	echo "$artist - $title"
+else
+	echo ""
+fi
